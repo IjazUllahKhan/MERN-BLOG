522205b55dfaea8dd95e16d76676d555b979fa5d
IjazUllahKhan <ijazullahkhan09@gmail.com>
2024-02-20 14:27:11 +0500

    update header component with user data

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import { signup,signin } from '../contollers/auth.controller.js';
+import { signup,signin,google } from '../contollers/auth.controller.js';
 
 const router = Router();
 
